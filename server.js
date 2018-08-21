d79a78d0970296dfc420a1805f0dd7009db12322
saurabh <saurabh.abhyankar7@gmail.com>
2018-08-21 11:47:41 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'))
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    console.log(`${now}: ${req.method} ${req.url}`); 
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs');
+})
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+app.get('/',(req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the New Website'
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    })
+});
+app.get('/bad', (req, res) => {
+    res.send({
+        error: "Unknown route."
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
